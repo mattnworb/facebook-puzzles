214cbc37bb848906d3092895193b3ad10c9a61f2
Matt Brown <msbcode@gmail.com>
2010-03-08 20:03:07 -0500

    switched to reading files with FileReader

--- breathalyzer/src/brown/puzzles/breathalyzer/CorpusInputParser.java
+++ breathalyzer/src/brown/puzzles/breathalyzer/CorpusInputParser.java
@@ -2,9 +2,8 @@ package brown.puzzles.breathalyzer;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import brown.puzzles.InputParser;
@@ -26,7 +25,7 @@ public class CorpusInputParser implements InputParser<List<String>> {
 		BufferedReader br = null;
 
 		try {
-			br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			br = new BufferedReader(new FileReader(file));
 
 			String line = null;
 
--- breathalyzer/src/brown/puzzles/breathalyzer/OrderedCorpusInputParser.java
+++ breathalyzer/src/brown/puzzles/breathalyzer/OrderedCorpusInputParser.java
@@ -2,9 +2,8 @@ package brown.puzzles.breathalyzer;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -31,7 +30,7 @@ public class OrderedCorpusInputParser implements InputParser<Map<Integer, List<S
 		BufferedReader br = null;
 
 		try {
-			br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			br = new BufferedReader(new FileReader(file));
 
 			String line = null;
 
