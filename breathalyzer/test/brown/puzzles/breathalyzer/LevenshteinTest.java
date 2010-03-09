d46d5551cec4ef4b6e1ec87487505eb04618f838
Matt Brown <msbcode@gmail.com>
2010-03-08 21:27:11 -0500

    forgot to fix a broken test after change to case insensitivity

--- breathalyzer/test/brown/puzzles/breathalyzer/LevenshteinTest.java
+++ breathalyzer/test/brown/puzzles/breathalyzer/LevenshteinTest.java
@@ -13,8 +13,10 @@ public class LevenshteinTest extends TestCase {
 	public void testScore() {
 		assertEquals(3, Levenshtein.score("sitting", "kitten"));
 		assertEquals(3, Levenshtein.score("Saturday", "Sunday"));
-		assertEquals(0, Levenshtein.score("this", "THIS"));
-		assertEquals(1, Levenshtein.score("goud", "GOUT"));
+		// removed case insensitivity from the score method, more performant to
+		// apply upper/lowercase rules at read time
+		// assertEquals(0, Levenshtein.score("this", "THIS"));
+		// assertEquals(1, Levenshtein.score("goud", "GOUT"));
 
 	}
 }
